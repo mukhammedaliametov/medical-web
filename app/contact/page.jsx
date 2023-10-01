28963aacb6ccfe9f92126940c3ec08667069eb52
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-10-01 21:07:54 +0500

    second commit ;)

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PageTitle from "@/components/PageTitle";
-// import YandexMap from "@/components/YandexMap";
+import YandexMap from "@/components/YandexMap";
 
 const page = () => {
   return (
     <>
       <PageTitle page="" link="Contact" url="" />
-      {/* <YandexMap /> */}
+      <YandexMap />
       <div>contact</div>
     </>
   );
--- components/YandexMap.jsx
+++ components/YandexMap.jsx
@@ -1,15 +1,23 @@
+'use client'
 import React from "react";
-import { YMaps, Map, Placemark } from "react-yandex-maps";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 const YandexMap = () => {
+
+  const defaultState = {
+    center: [55.751574, 37.573856],
+    zoom: 5,
+  };
+
   return (
-    <YMaps>
-      <div>
-        <Map defaultState={{ center: [42.836144, 59.008739], zoom: 6 }}>
-          <Placemark geometry={[42.836144, 59.008739]} />
-        </Map>
-      </div>
+    <div className='w-full p-8 md:p-16'>
+      <YMaps id='map'>
+      <Map defaultState={defaultState}>
+        <Placemark geometry={[55.684758, 37.738521]} />
+      </Map>
     </YMaps>
+    </div>
+    
   );
 };
 
